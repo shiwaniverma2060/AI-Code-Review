691b79b015af72c6431adbec660fd4baaff2c4e3
shiwaniverma2060 <shiwaniverma2060@gmail.com>
2026-06-06 23:20:28 +0530

    Refactor server.js to use environment PORT variable

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,7 @@
-require ('dotenv').config()
-const app = require('./src/app')
+const app = require("./src/app");
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,() =>{
-    console.log('server is running on http://localhost:3000')
-})
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
